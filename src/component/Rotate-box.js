f166d1aaeb080125b9b060a86343481d2ca32fc7
oyiooi <15055142531@163.com>
2019-07-31 17:46:56 +0800

    修改旋转广告

--- src/component/Rotate-box.js
+++ src/component/Rotate-box.js
@@ -5,7 +5,7 @@ function RotateBox () {
     return <div className='more-info'>
                    <div className='box-wrap'>
                        <div className='plane1'>高德地图API</div>
-                       <div className='plane2'>天气API:https://www.tianqiapi.com/</div>
+                       <div className='plane2'>天气API:<br />https://www.tianqiapi.com/</div>
                        <div className='plane3'>Iconfont</div>
                    </div>
             </div>     
